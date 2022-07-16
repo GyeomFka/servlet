77cb07dd717a3224832d92a9260f757ec8e6dc15
GyeomFka <clazziquai01@gmail.com>
2022-07-16 19:29:32 +0900

    request-body

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 - jar : 내장 톰캣 간단 빌드
 - war : 톰캣서버 별도 설정
 
+### HttpServletRequest 개요
+- HttpServletRequest, HttpServletResponse 객체들은 HTTP요청 메시지, HTTP응답 메시지를 
+편리하게 사용하도록 도와주는 객체
+
+### HTTP 요청 데이터 개요
+- GET - 쿼리 파라미터
+- POST - HTML Form
+- HTTP message body
---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,47 @@
+package hello.servlet.basic.request;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 1. 파라미터 전송
+ * http://localhost:8080/request-param?username=hello&age=20
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		printAllParameters(request);
+		printSingleParam(request);
+
+		response.getWriter().write("ok");
+
+	}
+
+	private void printAllParameters(HttpServletRequest request) {
+		System.out.println(" ::: request param servlet service ::: ");
+
+		request.getParameterNames().asIterator()
+				.forEachRemaining(parameterNames -> System.out.println(parameterNames
+					+ " : " + request.getParameter(parameterNames)));
+
+		System.out.println(" ::: request param servlet service ::: ");
+	}
+
+	private void printSingleParam(HttpServletRequest request) {
+		System.out.println(" :: 단일 파라미터 조회 :: ");
+
+		System.out.println(request.getParameter("username"));
+		System.out.println(request.getParameter("age"));
+
+		System.out.println(" :: 단일 파라미터 조회 :: ");
+	}
+
+}
